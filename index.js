57277c7931757558515ed8c4ec8e64dcf2d8db4d
lgabryal10 <lgabryal@gmail.com>
2026-09-04 08:16:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const app = express();
 
 let mode = "idle";
 
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
+// 🔥 YOUR OUTFITS (PUT IMAGE LINKS HERE)
+let outfits = [
+  {
+    top: "https://i.imgur.com/AAA.jpg",
+    bottom: "https://i.imgur.com/BBB.jpg",
+    shoes: "https://i.imgur.com/CCC.jpg"
+  }
+];
 
+// trigger
 app.get("/trigger/outfit", (req, res) => {
   mode = "outfit";
   res.send("Outfit mode ON");
 });
 
+// status + outfit data
 app.get("/status", (req, res) => {
-  res.json({ mode });
+  res.json({
+    mode,
+    outfit: outfits[0]
+  });
 });
 
 app.listen(3000, () => console.log("Running"));
